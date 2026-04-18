0b11391229d91fc251ad9b4ddf378d616e409dce
VidadiMurad <vidad0murad@gmail.com>
2026-04-18 18:13:55 +0400

    Calculate the answer

--- App.java
+++ App.java
@@ -15,14 +15,22 @@ public class App {
     double firstSide;
     double secondSide;
     double result;
+    double lastResult;
     
     // Labels for get data from user
     System.out.print("Enter First Side:");
     firstSide = scanner.nextDouble();
 
+    // Operations
     System.out.print("enter Second Side:");
     secondSide = scanner.nextDouble();
 
+    result = Math.pow(firstSide, 2) + Math.pow(secondSide, 2);
+    lastResult = Math.sqrt(result);
+
+
+    // Result
+    System.out.printf("The Answer is: %.1f\n", lastResult);
 
 
     scanner.close();
