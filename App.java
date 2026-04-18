1c25e8d25497116c355abd893dd1afdeb6a02bdf
VidadiMurad <vidad0murad@gmail.com>
2026-04-18 18:07:37 +0400

    Add Variables

--- App.java
+++ App.java
@@ -6,6 +6,23 @@ import java.util.Scanner;
 import java.util.Locale;
 
 public class App {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    scanner.useLocale(Locale.US);
+    
+    // Variables
+
+    double firstSide;
+    double secondSide;
+    double result;
+    
+    // Labels for get data from user
+    System.out.print("Enter First Side:");
+
+
+
+    scanner.close();
+    }
     
 }
 
